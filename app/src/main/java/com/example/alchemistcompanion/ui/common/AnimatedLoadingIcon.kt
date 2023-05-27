4da457ad612a36137b3f34abc76e0ba6cc33f257
Simon Staal <sts219@ic.ac.uk>
2023-05-27 18:45:23 +0100

    Refactored animated loading icon to be common composable

---
+++ app/src/main/java/com/example/alchemistcompanion/ui/common/AnimatedLoadingIcon.kt
@@ -0,0 +1,38 @@
+package com.example.alchemistcompanion.ui.common
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.alchemistcompanion.R
+
+@Composable
+fun AnimatedLoadingIcon(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val progress by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Image(
+        modifier = modifier
+            .graphicsLayer {
+                rotationZ = progress
+            },
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
--- app/src/main/java/com/example/alchemistcompanion/ui/setup/SetupScreen.kt
+++ app/src/main/java/com/example/alchemistcompanion/ui/setup/SetupScreen.kt
@@ -1,12 +1,6 @@
 package com.example.alchemistcompanion.ui.setup
 
 import android.util.Log
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -27,10 +21,8 @@ import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.alchemistcompanion.R
+import com.example.alchemistcompanion.ui.common.AnimatedLoadingIcon
 import com.example.alchemistcompanion.ui.theme.AlchemistCompanionTheme
 
 @Composable
@@ -143,29 +136,11 @@ fun ConnectionStatus(
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
-    val infiniteTransition = rememberInfiniteTransition()
-    val progress by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(3000, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        )
-    )
-
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
     ) {
-        Image(
-            modifier = Modifier
-                .size(200.dp)
-                .graphicsLayer {
-                    rotationZ = progress
-                },
-            painter = painterResource(id = R.drawable.loading_img),
-            contentDescription = stringResource(R.string.loading)
-        )
+        AnimatedLoadingIcon(modifier = Modifier.size(200.dp))
     }
 }
 
